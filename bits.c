641058bf229ac2a57e9d29f12e942604adcaeaeb
heriberto2300 <joalonsom@hotmail.com>
2019-01-08 20:04:57 -0600

    Mi primera version

---
+++ bits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(int argv, char** argc){
+	int valorUsuario;
+	int mascara=1;
+	int resultado;
+	int ciclo;
+	int cont=0;
+	printf("Introduzca un número");
+	scanf("%d", &valorUsuario);
+	for (ciclo=1; ciclo<=16; ciclo++){
+		resultado=valorUsuario&mascara;
+		if (resultado==1){
+			cont=cont+1;
+		}
+		valorUsuario=valorUsuario>>1;
+	}
+	printf("Bits prendidos:" "%d", cont);
+	
+	return 0;
+}
